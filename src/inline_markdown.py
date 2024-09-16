680dc882cf4da9edd3eead82e3613a10614cfdfa
Solu7ion <armanid03.02@mail.ru>
2024-09-16 14:23:09 +0500

    Add unit test inline_markdown

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,19 +1,38 @@
 from textnode import TextNode
+import re
 
+from textnode import(
+    TextNode, 
+    text_type_text,
+    text_type_bold,
+    text_type_code,
+    text_type_italic,
+)
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
-    for node in old_nodes:
-        if node.text_type != "text":
-            new_nodes.append(node)
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+            new_nodes.append(old_node)
             continue
-        
-        parts = node.text.split(delimiter)
-        if len(parts) % 2 == 0:
-            raise ValueError(f"Invalid Markdown syntax for delimiter {delimiter}")
-       
-        for i, part in enumerate(parts):
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
             if i % 2 == 0:
-                new_nodes.append(TextNode(part, "text"))
+                split_nodes.append(TextNode(sections[i], text_type_text))
             else:
-                new_nodes.append(TextNode(part, text_type))
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
     return new_nodes
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+    return matches
+
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
+    return matches
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -11,6 +11,12 @@ from textnode import (
     text_type_image
 )
 
+from inline_markdown import(
+    extract_markdown_links,
+    extract_markdown_images,
+    split_nodes_delimiter,
+)
+
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", "bold",)
@@ -31,5 +37,44 @@ class TestTextNodeToHtmlNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.props,  {"src": "https://img.com", "alt": "This is image"}, )
+
+
+class TextInlineMarkdown(unittest.TestCase):
+    def test_delim_code(self):
+        node = TextNode("This is text with a `code block` word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" word", text_type_text),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_bold_and_italic(self):
+        node = TextNode("**bold** and *italic*", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        new_nodes = split_nodes_delimiter(new_nodes, "*", text_type_italic)
+        self.assertEqual(
+            [
+                TextNode("bold", text_type_bold),
+                TextNode(" and ", text_type_text),
+                TextNode("italic", text_type_italic),
+            ],
+            new_nodes,
+        )
+
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        text2 = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(extract_markdown_images(text), text2)
+
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        text2 = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(extract_markdown_links(text), text2)
+
+
 if __name__ == "__main__":
     unittest.main()
