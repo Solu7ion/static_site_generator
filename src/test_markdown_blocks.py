a31a79de6482f5069460ff0a314236a6b0829dbf
Solu7ion <armanid03.02@mail.ru>
2024-09-17 14:36:44 +0500

    Add mardown_blocks and unit test

---
+++ src/test_markdown_blocks.py
@@ -0,0 +1,23 @@
+import unittest
+from markdown_blocks import markdown_to_blocks
+
+class TestMarkdownToHtml(unittest.TestCase):
+    def test_markdown_to_block(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+                "* This is a list\n* with items",            
+            ]
+        )
